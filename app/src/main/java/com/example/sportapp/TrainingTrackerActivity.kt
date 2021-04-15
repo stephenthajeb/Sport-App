0fb13335896432423a9ebac12cb548cfe46ef27d
stephenthajeb <13518150@std.stei.itb.ac.id>
2021-04-15 11:44:55 +0700

    core: implement compass sensor

--- app/src/main/java/com/example/sportapp/TrainingTrackerActivity.kt
+++ app/src/main/java/com/example/sportapp/TrainingTrackerActivity.kt
@@ -1,6 +1,10 @@
 package com.example.sportapp
 
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,11 +12,19 @@ import android.view.MenuItem
 import com.example.sportapp.databinding.ActivityTrainingTrackerBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlin.math.round
 
-class TrainingTrackerActivity : AppCompatActivity() {
+class TrainingTrackerActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var binding: ActivityTrainingTrackerBinding
+    private lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
 
-    companion object{
+
+
+    companion object {
         private var TAB_TITLES = arrayOf(
             TrainingTrackerFragment.RUNNING_MODE,
             TrainingTrackerFragment.CYCLING_MODE,
@@ -26,30 +38,107 @@ class TrainingTrackerActivity : AppCompatActivity() {
         setActiveNavMenu()
         menuItemListener()
         setUpTabLayout()
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
     }
 
-    private fun setUpTabLayout(){
+    override fun onResume() {
+        super.onResume()
+        activateCompassSensorListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    private fun activateCompassSensorListener(){
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(
+                this,
+                magneticField,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+    }
+
+    private fun getDirection(angle:Double):String{
+        var direction =""
+        if (angle >= 350 || angle <= 10)
+            direction = "N"
+        if (angle < 350 && angle > 280)
+            direction = "NW"
+        if (angle <= 280 && angle > 260)
+            direction = "W"
+        if (angle <= 260 && angle > 190)
+            direction = "SW"
+        if (angle <= 190 && angle > 170)
+            direction = "S"
+        if (angle <= 170 && angle > 100)
+            direction = "SE"
+        if (angle <= 100 && angle > 80)
+            direction = "E"
+        if (angle <= 80 && angle > 10)
+            direction = "NE"
+
+        return direction
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        //ToDo: register listener on another thread
+        if (event == null) {
+            return
+        }
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+        //Update sensor data to UI
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
+        val orientation = SensorManager.getOrientation(rotationMatrix, orientationAngles)
+        val degrees = (Math.toDegrees(orientation[0].toDouble()) + 360.0) % 360.0
+        val angle = round(degrees * 100) / 100
+        val direction = getDirection(degrees)
+        binding.tvDegree.text = "$angle $direction"
+        binding.ivCompass.rotation = angle.toFloat() * -1
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    private fun setUpTabLayout() {
         val tabLayoutAdapter = TrainingTrackerFragmentAdapter(this)
         binding.viewPager.adapter = tabLayoutAdapter
-        TabLayoutMediator(binding.tabs,binding.viewPager){ tab,position->
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
             tab.text = TAB_TITLES[position]
         }.attach()
         supportActionBar?.elevation = 0f
     }
 
-    private fun setActiveNavMenu(){
-        val menuItem : MenuItem = binding.bottomNavView.menu.getItem(1)
+    private fun setActiveNavMenu() {
+        val menuItem: MenuItem = binding.bottomNavView.menu.getItem(1)
         menuItem.isChecked = true
     }
 
-    private fun menuItemListener(){
-        binding.bottomNavView.setOnNavigationItemSelectedListener {item->
-            when(item.itemId){
-                R.id.nav_news->{
-                    val intent = Intent(this,NewsActivity::class.java )
+    private fun menuItemListener() {
+        binding.bottomNavView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_news -> {
+                    val intent = Intent(this, NewsActivity::class.java)
                     startActivity(intent)
                 }
-                R.id.nav_tracker->{
+                R.id.nav_tracker -> {
                     return@setOnNavigationItemSelectedListener true
                 }
             }
--- app/src/main/java/com/example/sportapp/TrainingTrackerFragment.kt
+++ app/src/main/java/com/example/sportapp/TrainingTrackerFragment.kt
@@ -1,11 +1,16 @@
 package com.example.sportapp
 
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.sportapp.databinding.FragmentTrainingTrackerBinding
 
 private const val TRAINING_MODE = "training_mode"
@@ -15,12 +20,13 @@ class TrainingTrackerFragment : Fragment() {
     private var trainingMode: String? = null
     private var binding: FragmentTrainingTrackerBinding? = null
 
+
     companion object {
         const val RUNNING_MODE = "running"
         const val CYCLING_MODE = "cycling"
 
         @JvmStatic
-        fun newInstance(position:Int) =
+        fun newInstance(position: Int) =
             TrainingTrackerFragment().apply {
                 trainingMode = if (position === 0) RUNNING_MODE else CYCLING_MODE
                 arguments = Bundle().apply {
@@ -29,12 +35,17 @@ class TrainingTrackerFragment : Fragment() {
             }
     }
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             trainingMode = it.getString(TRAINING_MODE)
         }
     }
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,9 +58,9 @@ class TrainingTrackerFragment : Fragment() {
 
     private fun showTrainingMode() {
         if (binding!==null){
-            Log.d("test","b")
+            Log.d("test", "b")
             if (trainingMode == RUNNING_MODE) {
-                Log.d("test","c")
+                Log.d("test", "c")
                 binding!!.tvTrainingModeIndicator.setCompoundDrawablesWithIntrinsicBounds(
                     0,
                     R.drawable.ic_baseline_directions_run_50,
@@ -58,7 +69,7 @@ class TrainingTrackerFragment : Fragment() {
                 )
                 binding!!.tvTrainingModeIndicator.text ="Running Mode"
             } else {
-                Log.d("test","d")
+                Log.d("test", "d")
                 binding!!.tvTrainingModeIndicator.setCompoundDrawablesWithIntrinsicBounds(
                     0,
                     R.drawable.ic_baseline_directions_bike_50,
@@ -69,4 +80,5 @@ class TrainingTrackerFragment : Fragment() {
             }
         }
     }
+
 }
