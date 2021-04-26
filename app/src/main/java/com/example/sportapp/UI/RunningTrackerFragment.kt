ecf3f82993767f3d1a3b51d1aa7e7825c0649ade
stephenthajeb <13518150@std.stei.itb.ac.id>
2021-04-27 01:25:49 +0700

    core: insert history working(contains bug)

--- app/src/main/java/com/example/sportapp/UI/RunningTrackerFragment.kt
+++ app/src/main/java/com/example/sportapp/UI/RunningTrackerFragment.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,28 +19,32 @@ import com.example.sportapp.Data.History
 import com.example.sportapp.Service.RunningTrackerService
 import com.example.sportapp.UI.Adapter.TrainingTrackerFragmentAdapter
 import com.example.sportapp.databinding.FragmentRunningTrackerBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RunningTrackerFragment : Fragment() {
     private var isTraining: Boolean = false
     private var steps: Float = 0f
     private var binding: FragmentRunningTrackerBinding? = null
-    private val historyViewModel: HistoryViewModel by viewModels{
+    private var startDate: Calendar? = null
+    private val historyViewModel: HistoryViewModel by viewModels {
         HistoryModelFactory((activity?.application as SportApp).historyDAO)
     }
 
 
-    companion object{
-        const val EXTRA_STEPS="steps"
+    companion object {
+        const val EXTRA_STEPS = "steps"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (activity !== null) LocalBroadcastManager.getInstance(requireActivity()).registerReceiver(
-            trackerReceiver,
-            IntentFilter(RunningTrackerService.ACTION_TRACKING)
-        )
-        if (savedInstanceState != null){
-            steps = savedInstanceState.getFloat(EXTRA_STEPS,0f)
+        if (activity !== null) LocalBroadcastManager.getInstance(requireActivity())
+            .registerReceiver(
+                trackerReceiver,
+                IntentFilter(RunningTrackerService.ACTION_TRACKING)
+            )
+        if (savedInstanceState != null) {
+            steps = savedInstanceState.getFloat(EXTRA_STEPS, 0f)
             isTraining = savedInstanceState.getBoolean(RunningTrackerService.EXTRA_IS_TRAINING)
         }
     }
@@ -74,17 +79,18 @@ class RunningTrackerFragment : Fragment() {
     }
 
 
-    private fun setForegroundTracking(activate:Boolean){
+    private fun setForegroundTracking(activate: Boolean) {
         val intent = Intent(activity, RunningTrackerService::class.java)
-        intent.putExtra(RunningTrackerService.EXTRA_IS_TRAINING,isTraining)
-        if (isTraining){
+        intent.putExtra(RunningTrackerService.EXTRA_IS_TRAINING, isTraining)
+        if (isTraining) {
             intent.putExtra(RunningTrackerService.EXTRA_IS_FOREGROUND, activate)
             context?.startService(intent)
         }
     }
 
     override fun onDestroy() {
-        if (activity !== null) LocalBroadcastManager.getInstance(requireActivity()).unregisterReceiver(trackerReceiver)
+        if (activity !== null) LocalBroadcastManager.getInstance(requireActivity())
+            .unregisterReceiver(trackerReceiver)
         super.onDestroy()
     }
 
@@ -93,25 +99,41 @@ class RunningTrackerFragment : Fragment() {
         binding?.btnTraining?.setOnClickListener {
             isTraining = !isTraining
             val intent = Intent(context, RunningTrackerService::class.java)
-            intent.putExtra(RunningTrackerService.EXTRA_IS_FOREGROUND,false)
-            intent.putExtra(RunningTrackerService.EXTRA_IS_TRAINING,isTraining)
+            intent.putExtra(RunningTrackerService.EXTRA_IS_FOREGROUND, false)
+            intent.putExtra(RunningTrackerService.EXTRA_IS_TRAINING, isTraining)
             context?.startService(intent)
             if (isTraining) {
-                Toast.makeText(context,"Start training: walking/running",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Start training: walking/running", Toast.LENGTH_SHORT)
+                    .show()
+                startDate = Calendar.getInstance()
                 binding?.btnTraining?.text = "Finish now"
             } else {
-                Toast.makeText(context,"Saving training record",Toast.LENGTH_SHORT).show()
-                binding?.btnTraining?.text = "Start Again"
-                binding?.tvProgress?.text = ""
-                try{
+                //Todo: delete later
+                //binding?.btnTraining?.text = "Start Again"
+                //binding?.tvProgress?.text = ""
+                Toast.makeText(context, "Saving training record", Toast.LENGTH_SHORT).show()
+                try {
+                    val date = SimpleDateFormat("yyyy-MM-dd",Locale.getDefault()).format(startDate?.time)
+                    val startTime = SimpleDateFormat("HH:mm",Locale.getDefault()).format(startDate?.time)
+                    val endTime = SimpleDateFormat("HH:mm",Locale.getDefault()).format(Calendar.getInstance().time)
+                    Log.d("test",date.toString())
+                    Log.d("test",startTime.toString())
+                    Log.d("test",endTime.toString())
                     //Todo: add other field, navigate to historyDetailTraining
-                    val history = History(mode=SchedulerAddActivity.RUNNING,result=steps)
+                    val history = History(
+                        mode = SchedulerAddActivity.RUNNING,
+                        result = steps,
+                        date = date,
+                        startTime = startTime,
+                        endTime = endTime
+                    )
                     historyViewModel.insert(history)
-                    val intent = Intent(context,HistoryDetailTrainingActivity::class.java)
-                    intent.putExtra(HistoryDetailFragment.EXTRA_HISTORY,history)
+                    val intent = Intent(context, HistoryDetailTrainingActivity::class.java)
+                    intent.putExtra(HistoryDetailFragment.EXTRA_HISTORY, history)
                     startActivity(intent)
-                }catch(e:Error){
-                    Toast.makeText(context,"Error in saving this record", Toast.LENGTH_SHORT).show()
+                } catch (e: Error) {
+                    Toast.makeText(context, "Error in saving this record", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
         }
@@ -120,7 +142,7 @@ class RunningTrackerFragment : Fragment() {
     private val trackerReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             steps =
-                intent.getFloatExtra(RunningTrackerService.STEPS_TRACKED,steps)
+                intent.getFloatExtra(RunningTrackerService.STEPS_TRACKED, steps)
             binding?.tvProgress?.text = "$steps steps"
         }
     }
