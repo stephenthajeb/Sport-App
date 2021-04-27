b43afa186244c7180ced882fcf3b73a203656fcc
Muchammad Ibnu <13518072@std.stei.itb.ac.id>
2021-04-27 22:21:05 +0700

    core: add gps and draw polyline

--- app/src/main/java/com/example/sportapp/Constant/Constant.kt
+++ app/src/main/java/com/example/sportapp/Constant/Constant.kt
@@ -1,5 +1,7 @@
 package com.example.sportapp.Constant
 
+import android.graphics.Color
+
 object Constant {
 
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
@@ -13,4 +15,9 @@ object Constant {
 
     const val LOCATION_UPDATE_INTERVAL = 5000L
     const val FASTEST_LOCATION_INTERVAL = 2000L
+
+    const val POLYLINE_COLOR = Color.RED
+    const val POLYLINE_WIDTH = 8f
+    const val MAP_ZOOM = 15f
+
 }
--- app/src/main/java/com/example/sportapp/Service/CyclingTrackerService.kt
+++ app/src/main/java/com/example/sportapp/Service/CyclingTrackerService.kt
@@ -76,10 +76,12 @@ class CyclingTrackerService : LifecycleService(){
                         isFirstRun = false
                     } else {
                         Timber.d("Resuming service...")
+                        startForegroundService()
                     }
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
+                    pauseService()
                 }
                 ACTION_STOP_SERVICE -> {
                     Timber.d("Stopped service")
@@ -87,6 +89,10 @@ class CyclingTrackerService : LifecycleService(){
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun pauseService() {
+        isTracking.postValue(false)
     }
 
     @SuppressLint("MissingPermission")
